693e7a0055bb2499d011715d4d07b4c1e8550ec0
alfugin37 <al.fugin@yandex.ru>
2023-01-10 22:09:59 +0300

    Added Example005_CondifElse

---
+++ Example005_CondifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿// Продолжаем обучение.
+Console.Write("Введите имя пользовотеля: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша! ");
+}
+else 
+{
+    Console.WriteLine("привет, ");
+    Console.WriteLine(username);
+}
